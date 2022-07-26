bb02f56291581bd6c9e8ed6d66f6aa2ba7b03bbd
omnia mahmoud <Omnia@SOFCO.org>
2022-07-26 20:49:13 +0200

    add expense date component

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 function ExpenseItem(props) {
-   const month=props.date.toLocaleString('en-US',{month:'long',})
-   const day=props.date.toLocaleString('en-US',{day:'2-digit',})
-   const year=props.date.getFullYear()
+ 
   return (
     <div className="expenceitem">
       <div className="expenceitem-left">
-        <h4 className="expenceitem-date">
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
-        </h4>
+       
+          <ExpenseDate date={props.date}/>
+      
         <h4 className="expenceitem-title">{props.title}</h4>
       </div>
       <div>
