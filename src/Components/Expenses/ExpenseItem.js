f61dc55a513161c53e09747ad176cb0e7010db53
omnia mahmoud <Omnia@SOFCO.org>
2022-08-02 09:12:26 +0200

    new commit

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,19 +1,27 @@
+import React ,{useState} from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 const ExpenseItem = (props) =>{
+  const [title,setTitle]=useState(props.title);
  
+ const eventHandler=()=>{
+  setTitle("updated")
+  console.log(props.title)
+ }
   return (
     <Card className="expenceitem">
       <div className="expenceitem-left">
        
           <ExpenseDate date={props.date}/>
       
-        <h4 className="expenceitem-title">{props.title}</h4>
+        <h4 className="expenceitem-title">{title}</h4>
       </div>
       <div>
         <button className="expenceitem-price">${props.amount}</button>
+        <button onClick={eventHandler}>change title</button>
       </div>
+     
     </Card>
   );
 }
